73a94792dd49f4dfde909c98a33161ca7ffecc27
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-07-14 02:50:55 +0500

    Final

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Style from './Header.module.css';
-import {Typography} from '@material-ui/core';
-
 const Header = () => {
   return (
     <div className={Style.box}>
       <div className={Style.title}>
-      <Typography variant="h5">Covid Tracker </Typography>
+      Covid Tracker 
             </div>
     </div>
   )
